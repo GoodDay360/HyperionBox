7a89455e9447e101178883ad14b4ca63d7049f69
unknown <istartgame31@gmail.com>
2025-03-11 20:52:23 +0700

    making progress + update manifest

--- src/splash_screen/scripts/check_node.tsx
+++ src/splash_screen/scripts/check_node.tsx
@@ -1,29 +1,50 @@
-import axios from "axios";
+import { fetch } from '@tauri-apps/plugin-http'
 import { info, error } from '@tauri-apps/plugin-log';
 import { platform, arch } from '@tauri-apps/plugin-os';
+import { resolve } from 'path';
 
 
 const check_node = async ({setFeedback}:any) => {
     info(await arch() + await platform())
 
     setFeedback("Downloading Node...")
-    axios({
-        url: "https://raw.githubusercontent.com/GoodDay360/HyperionBox/refs/heads/main/manifest.json",
-        method: "get"
+
+    const manifest:any = await new Promise((resolve,reject)=>{
+        fetch("https://raw.githubusercontent.com/GoodDay360/HyperionBox/refs/heads/main/manifest.json", {
+            method: 'GET'
+        })
+        .then(async (res)=>{
+            const DATA = await res.json()
+            resolve(DATA)
+        })
+        .catch((e)=>{
+            error(e)
+            reject(e)
+        })
     })
-    .then(async (response) => {
-        const node_manifest = response.data.node;
+    const node = manifest?.files?.node;
 
-        const node_url = node_manifest?.[await platform()]?.[await arch()];
-        if (!node_url) setFeedback("Your system doesn't support this app.")
-        info(node_url)
+    const node_url = node?.[await platform()]?.[await arch()];
+    if (!node_url) setFeedback("Your system doesn't support this app.")
+    info(node_url)
+
+    // axios({
+    //     url: "https://raw.githubusercontent.com/GoodDay360/HyperionBox/refs/heads/main/manifest.json",
+    //     method: "get"
+    // })
+    // .then(async (response) => {
+    //     const node_manifest = response.data.files.node;
+
+    //     const node_url = node_manifest?.[await platform()]?.[await arch()];
+    //     if (!node_url) setFeedback("Your system doesn't support this app.")
+    //     info(node_url)
 
 
 
-    })
-    .catch(error => {
-        console.error('Error fetching the data:', error);
-    });
+    // })
+    // .catch(error => {
+    //     console.error('Error fetching the data:', error);
+    // });
 }
 
 export default check_node;
