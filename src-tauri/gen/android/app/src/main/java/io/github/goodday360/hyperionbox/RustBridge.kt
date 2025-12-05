b085d6226c92d738c113e4f4461fd7a4f3094773
unknown <istartgame31@gmail.com>
2025-12-05 13:30:57 +0700

    Revert "add navigate back in view"
    This reverts commit ee0ecb1e5842d249ff1b424bd8589960043d7b94.

--- src-tauri/gen/android/app/src/main/java/io/github/goodday360/hyperionbox/RustBridge.kt
+++
@@ -1,9 +0,0 @@
-package io.github.goodday360.hyperionbox
-
-object RustBridge {
-    external fun onPermissionResult(granted: Boolean)
-
-    init {
-        System.loadLibrary("hyperionbox_lib") 
-    }
-}
